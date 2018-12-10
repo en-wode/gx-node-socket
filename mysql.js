df63a8127f7d78dba46f6f66bd682827e88fe74d
en-wode <875957823@qq.com>
2018-12-10 13:18:15 +0800

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql');
 var pool = mysql.createPool({
-  host     : '47.98.162.168',
-  user     : 'root',
-  password : '123456',
-  database : 'lanxiang'
+  host     : '',
+  user     : '',
+  password : '',
+  database : ''
 });
 
 module.exports =pool
